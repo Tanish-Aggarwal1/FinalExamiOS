e18be27b6cb493adde5c4748f612d77e2223861b
Tanish Aggarwal <atanish6@gmail.com>
2026-08-04 12:51:48 -0400

    Initial Commit

---
+++ AggataniFinalExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AggataniFinalExam
+//
+//  Created by Tanish Aggarwal on 2026-08-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
